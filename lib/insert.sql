9e4b7e7c52d8fb71bf0a707f8cabeb873b3bfb4c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-19 20:27:02 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bear(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, M, "brown", "Wiley", true);
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, M, "brown", "Wiley", true);
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, F, "brown", "Wiley", true);
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, M, "brown", "Wiley", true);
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, MF "brown", "Wiley", true);
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, M, "brown", "Wiley", true);
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, F "brown", "Wiley", true);
-INSERT INTO (name, age, gender, color, temperament, alive) VALUES (NULL, 4, M, "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, M, "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, M, "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, F, "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, M, "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, MF "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, M, "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, F "brown", "Wiley", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, M, "brown", "Wiley", true);
